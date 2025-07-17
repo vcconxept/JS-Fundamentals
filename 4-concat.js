b5f3f00ea286f0ba162f0bf76586a89c9fb816f9
Viktor Charles Offori <148826771+vcconxept@users.noreply.github.com>
2025-07-17 22:44:41 +0000

    Add files via upload

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,13 @@
+#!/usr/bin/node
+
 const args = process.argv;
 
-if (args[2] === undefined || args[3] === undefined) {
-  console.log("Two arguments are required");
+if (args[2] === undefined) {
+  // No arguments, print nothing
+} else if (args[3] === undefined) {
+  // One argument
+  console.log(args[2]);
 } else {
+  // Two arguments
   console.log(`${args[2]} is ${args[3]}`);
 }
